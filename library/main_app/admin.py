7b7d912b8b7821c234490d610136021324e95e4d
bvddw <galaid.arseniy@gmail.com>
2023-08-30 01:12:23 +0300

    distributed models for admin page

--- library/main_app/admin.py
+++ library/main_app/admin.py
@@ -1,12 +1,4 @@
 from django.contrib import admin
 from .models import MyUser
-from genres.models import Genre
-from authors.models import Author
-from books.models import Book
-from borrow_requests.models import BorrowRequestModel
 
 admin.site.register(MyUser)
-admin.site.register(Genre)
-admin.site.register(Author)
-admin.site.register(Book)
-admin.site.register(BorrowRequestModel)
